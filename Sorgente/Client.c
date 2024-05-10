dce1ac773e24b3f543a708c62259350fc23dc7e1
TaleWind28 <matteoicy2@gmail.com>
2024-05-10 12:23:49 +0200

    Ottimizzazione Occupazione in memoria nella funzione Play lato Client

--- Sorgente/Client.c
+++ Sorgente/Client.c
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]){
     struct sockaddr_in server_address;
     socklen_t server_len = sizeof(server_address);
     /*INIZIALIZZAZIONE VARIABILI IN BASE ALL'ARGOMENTO DELLA RIGA DI COMANDO*/
-    //char* HOST = argv[1];
+    char* HOST = argv[1];
     int PORT = atoi(argv[2]);
 
     /*CONTROLLO PARAMETRI RIGA DI COMANDO*/
@@ -34,11 +34,11 @@ int main(int argc, char* argv[]){
     }
     /*inizializzazione server_address*/
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(PORT);
+    // server_address.sin_port = htons(PORT);
     server_address.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
-    // /*PROVA INTERNET*/
-    // server_address.sin_addr.s_addr = inet_addr("192.168.228.171");
-    // server_address.sin_port = htons(20000);
+    /*PROVA INTERNET*/
+    //server_address.sin_addr.s_addr = inet_addr(HOST);
+    server_address.sin_port = htons(PORT);
     
     /*creo il socket per comunicare*/
     SYSC(client_fd,socket(AF_INET,SOCK_STREAM,0),"nella creazione del client");
@@ -55,14 +55,13 @@ int main(int argc, char* argv[]){
 
 void Play(int client_fd){
     /*DICHIARAZIONE VARIABILI*/
-    int retvalue,i = 0;ssize_t n_read;char score[buff_size],matrice[buff_size];
-    
-    /*LEGGO LA MATRICE PASSATA DAL SERVER*/
-    SYSC(retvalue,read(client_fd,matrice,buff_size),"nella lettura della matrice");
+    int retvalue,i = 0;ssize_t n_read;char score[buff_size],matrice_stringa[buff_size];
     /*CREO LA MATRICE DA FAR UTILIZZARE AL CLIENT*/
     Matrix client_matrix = Create_Matrix(NUM_ROWS,NUM_COLUMNS);
+    /*LEGGO LA MATRICE PASSATA DAL SERVER*/
+    SYSC(retvalue,read(client_fd,matrice_stringa,buff_size),"nella lettura della matrice");
     /*RIEMPIO LA MATRICE COI DATI DEL SERVER*/
-    Fill_Matrix(client_matrix,matrice);
+    Fill_Matrix(client_matrix,matrice_stringa);
     /*COSTRUISCO LA MAPPA DEI CARATTERI DELLA MATRICE*/
     Build_Charmap(client_matrix);
     /*STAMPO LA MATRICE A SCHERMO*/
@@ -73,18 +72,18 @@ void Play(int client_fd){
         if (i == 5)break;
         /*STAMPA A SCHERMO*/
         writef(retvalue,"Inserisci una parola\n");
-        /*OTTIMIZZARE*/
-        char** input = (char**)malloc(buff_size*(sizeof(char*)));
-        input[i] =(char*)malloc(buff_size*(sizeof(char)));
+        /*ALLOCO UN PUNTATORE A CARATTERI DOVE MEMORIZZARE GLI INPUT UTENTE*/
+        char* input = (char*)malloc(buff_size*sizeof(char));
         /*ASPETTO UN INPUT DALL'UTENTE*/
-        SYSC(n_read,read(STDIN_FILENO,input[i],buff_size),"nella lettura dell'input del cliente");
+        SYSC(n_read,read(STDIN_FILENO,input,buff_size),"nella lettura dell'input del cliente");
+        retvalue = realloc(input,strlen(input)*sizeof(char));
         /*CONTROLLO SE L'INPUT È ACCETTABILE*/
-        if(strlen(input[i]) == 1){
-            input[i] = "lol\n";
-            n_read = strlen(input[i]);
+        if(strlen(input) == 1){
+            input = "lol\n";
+            n_read = strlen(input);
         }
         /*SCRIVO SUL FILE DESCRIPTOR CONDIVISO COL SERVER IL MESSAGGIO DA MANDARE*/
-        SYSC(retvalue,write(client_fd,input[i],n_read),"nella write");
+        SYSC(retvalue,write(client_fd,input,n_read),"nella write");
         /*ASPETTO CHE MI VENGA COMUNICATO IL PUNTEGGIO*/
         SYSC(n_read,read(client_fd,score,buff_size),"nella lettura del punteggio");
         /*SCRIVO A VIDEO IL PUNTEGGIO*/
