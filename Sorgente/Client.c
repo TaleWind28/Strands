c999c01dbd2b4ce8d786890b603f50ffd03554a6
TaleWind <matteomartini28@outlook.it>
2024-05-16 22:16:43 +0200

    Prova Esplosione Git

--- Sorgente/Client.c
+++ Sorgente/Client.c
@@ -42,56 +42,7 @@ int main(int argc, char* argv[]){
         
     /*chiamo la connect*/
     SYSC(retvalue,connect(client_fd,(struct sockaddr*)&server_address,server_len),"nella connect");
-    
-    /*CREO LA MATRICE DA FAR UTILIZZARE AL CLIENT*/
-    Matrix client_matrix = Create_Matrix(NUM_ROWS,NUM_COLUMNS);
-
-    /*LEGGO LA MATRICE PASSATA DAL SERVER*/
-    strcpy(matrice_stringa,Receive_Message(client_fd,msg_type));
-    /*RIEMPIO LA MATRICE COI DATI DEL SERVER*/
-    Fill_Matrix(client_matrix,matrice_stringa);
-    /*COSTRUISCO LA MAPPA DEI CARATTERI DELLA MATRICE*/
-    Build_Charmap(client_matrix);
-    /*STAMPO LA MATRICE A SCHERMO*/
-    Print_Matrix(client_matrix,'Q','u');
-    /*GIOCO LA PARTITA*/
-    Play(client_fd);
-
+    writef(retvalue,"connesso\n");
     /*chiudo il socket*/
     SYSC(retvalue,close(client_fd),"chiusura del cliente");
-}
-
-void Play(int client_fd){
-    /*DICHIARAZIONE VARIABILI*/
-    int retvalue;ssize_t n_read;char msg_type = '0';
-    
-    /*IL CLIENT PUO INIZIARE A GIOCARE*/
-    while (1){
-        /*TERMINAZIONE DELLA PARTITA DA CAMBIARE QUANDO AVRò LA SPECIFICA*/
-        /*STAMPA A SCHERMO*/
-        writef(retvalue,"Prompt Paroliere\n");
-        /*ALLOCO UN PUNTATORE A CARATTERI DOVE MEMORIZZARE GLI INPUT UTENTE*/
-        char* input = (char*)malloc(buff_size);
-        /*ASPETTO UN INPUT DALL'UTENTE*/
-        SYSC(n_read,read(STDIN_FILENO,input,buff_size),"nella lettura dell'input del cliente");
-        /*CONTROLLO SE L'UTENTE HA PER SBAGLIO INSERITO UN \n COME PAROLA*/
-        if(n_read == 1){
-            strcpy(input,"l\n");
-            n_read = 1;
-        }
-        char* token = strtok(input,"\n"); 
-        Caps_Lock(token);
-        /*SCRIVO SUL FILE DESCRIPTOR CONDIVISO COL SERVER IL MESSAGGIO DA MANDARE*/
-        Send_Message(client_fd,token,MSG_PAROLA);
-        /*ASPETTO CHE MI VENGA COMUNICATO IL PUNTEGGIO*/
-        char* score = Receive_Message(client_fd,msg_type);
-        /*SCRIVO A VIDEO IL PUNTEGGIO*/
-        SYSC(retvalue,write(STDOUT_FILENO,score,strlen(score)),"nella comunicazione del punteggio a video");
-        free(input);
-    }
-    /*leggo il totale*/
-    char* total_score = Receive_Message(client_fd,msg_type);
-    /*COMUNICO A VIDEO IL PUNTEGGIO TOTALE*/
-    SYSC(retvalue,write(STDOUT_FILENO,total_score,strlen(total_score)),"nella comunicazione del punteggio totale");
-    return;
 }
--- Sorgente/Server.c
+++ Sorgente/Server.c
@@ -26,26 +26,6 @@
 //#define MATRICI "../File Testuali/Matrici.txt"
 
 void* Thread_Handler(void* );
-void* Dealer(void *);
-void* Pignoler(void *);
-void Play(int );
-void Build_Dictionary(Hash_Entry* ,char* );
-
-Matrix playing_matrix;
-Hash_Entry Dictionary[TABLE_SIZE];
-
-void print_hash_table(Hash_Entry *table, unsigned int Table_size) {
-    printf("Contenuto della tabella hash:\n");
-    printf("-----------------------------\n");
-    for (int i = 0; i < Table_size; i++) {
-        if (table[i].is_occupied) {
-            printf("Indice %d: %s\n", i, table[i].string);
-        } else {
-            printf("Indice %d: vuoto\n", i);
-        }
-    }
-    printf("-----------------------------\n");
-}
 
 int main(int argc, char* argv[]){
     /*CONTROLLO PARAMETRI RIGA DI COMANDO*/
@@ -70,10 +50,7 @@ int main(int argc, char* argv[]){
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = inet_addr(HOST);
     server_address.sin_port = htons(PORT);
-    /*inizializzo la tabella hash dove memorizzare il dizionario*/
-    init_table(Dictionary,TABLE_SIZE);
-    /*CREAZIONE DIZIONARIO*/
-    Build_Dictionary(Dictionary,DIZIONARIO);
+
     /*creazione socket*/
     SYSC(server_fd,socket(AF_INET,SOCK_STREAM,0),"creazione socket del server");
     
@@ -82,8 +59,6 @@ int main(int argc, char* argv[]){
     
     /*predisposizione del listner*/
     SYSC(retvalue,listen(server_fd,MAX_NUM_CLIENTS),"nella listen");
-    /*CREO UN THREAD PER REGOLAMENTARE LE PARTITE*/
-    SYST(retvalue,pthread_create(&dealer_thread,NULL,Dealer,MATRICI),"nella creazione dell'arbitro");
 
     for(int i=0;i<MAX_NUM_CLIENTS;i++){
         /*accettazione delle richieste*/
@@ -95,8 +70,6 @@ int main(int argc, char* argv[]){
     for(int i =0;i<MAX_NUM_CLIENTS;i++){
         SYST(retvalue,pthread_join(client_thread[i],NULL),"nella join dei dispatcher");
     }
-    /*ATTESA DEL THREAD DEALER*/
-    SYST(retvalue,pthread_join(dealer_thread,NULL),"attesa dealer");
     /*CHIUSURA DEL SOCKET*/
     SYSC(retvalue,close(server_fd),"chiusura server");
     return 0;
@@ -105,151 +78,8 @@ int main(int argc, char* argv[]){
 void* Thread_Handler(void* args){
     int retvalue;
     int client_fd = *(int*) args;
-    /*faccio giocare il client*/
-    Play(client_fd);
     writef(retvalue,"fine player\n");
     /*chiusura del socket*/
     SYSC(retvalue,close(client_fd),"chiusura client-fake");
     return NULL;
-}
-
-void* Dealer(void * args){
-    int retvalue;
-    char* MATRICI = (char*)args;
-    /*attesa pre partita*/
-    playing_matrix = Create_Matrix(NUM_COLUMNS,NUM_ROWS);
-    /*Inizializza la matrice in base ad una riga del file matrici*/
-    Load_Matrix(playing_matrix,MATRICI,'U');
-    /*MAPPATURA DEI CARATTERI DELLA MATRICE*/
-    Build_Charmap(playing_matrix);
-    //Print_Matrix(playing_matrix);
-    /*appena finisce l'attesa lo comunica ai thread*/
-    printf("attesa finita\n");
-    /*durata della partita*//*mettere una alarm*/
-    sleep(30);
-    /*aspetta che finisca la partita*/
-    writef(retvalue,"fine partita\n");
-    /*loop*/
-    return NULL;
-}
-
-void Play(int client_fd){
-    int retvalue, point = 0,highscore = 0,exit_val = 0;
-    /*creo una tabella hash per memorizzare le parole indovinate dall'utente*/
-    Hash_Entry parole_indovinate[GUESSED_SIZE];
-    /*inizializzo la tabella hash*/
-    init_table(parole_indovinate,GUESSED_SIZE);
-    
-    pthread_t thread_pignoler;
-    /*ALLOCO UNA STRINGA DOVE ANDRO A MEMORIZARE LA MATRICE*/
-    char* string_matrix = (char*)malloc(playing_matrix.size*(sizeof(char)));
-    /*RENDO LA MATRICE DI GIOCO UNA STRINGA*/
-    Stringify_Matrix(playing_matrix,string_matrix);
-    writef(retvalue,"client connesso\n");
-
-    /*MANDO LA MATRICE AL CLIENT*/
-    char msg_type = MSG_OK;
-    Send_Message(client_fd,string_matrix,MSG_MATRICE);
-    while(1){
-        /*LETTURA DAL CLIENT*/
-        char* score = (char*)malloc(buff_size*sizeof(char));
-        char* input = Receive_Message(client_fd,msg_type);
-        /*LANCIO IL PIGNOLER*/
-        SYST(retvalue,pthread_create(&thread_pignoler,NULL,Pignoler,(void*)input),"nella creazione del pignoler");
-        Adjust_String(input,'U');
-        /*CONTROLLO DELLA VALIDITà DELLA PAROLA*/
-        if (Validate(playing_matrix,input)==0){
-            /*ASPETTO IL PIGNOLER*/
-            SYST(retvalue,pthread_join(thread_pignoler,(void**)&exit_val),"nell'attesa del pignoler con parola valida");
-            if (search_string(parole_indovinate,input,GUESSED_SIZE)== -1 && exit_val == 0){
-                insert_string(parole_indovinate,input,GUESSED_SIZE);
-                /*CALCOLO IL PUNTEGGIO DELLA PAROLA*/
-                point = strlen(input);
-                /*MESSAGGIO LATO SERVER*/
-                writef(retvalue,"parola legale\n");
-                /*FORMATTAZZIONE PER LA STRINGA DA PASSARE AL CLIENT*/
-                sprintf(score,"%d punti,complimenti\n",point);
-                
-            }else{
-                /*ASSEGNO 0 PUNTI PERCHÈ LA PAROLA È GIA STATA USATA*//*stampa di debug lato server*/
-                writef(retvalue,"parola già inserita\n");
-                /*FORMATTAZZIONE PER LA STRINGA DA PASSARE AL CLIENT*/
-                sprintf(score,"0 punti,ci hai provato brighella\n");
-            }
-        }else{/*PAROLA NON VALIDA*/
-            /*stampa lato server per debug*/
-            writef(retvalue,"parola illegale\n");
-            /*FORMATTAZZIONE PER LA STRINGA DA PASSARE AL CLIENT*/
-            sprintf(score,"0 punti,sagace\n");
-            /*ASPETTO IL PIGNOLER*/
-            SYST(retvalue,pthread_join(thread_pignoler,(void**)&exit_val),"nell'attesa del pignoler con la parola non valida");
-            /*ASSEGNO 0 PUNTI PERCHÈ LA PAROLA NON È VALIDA*/
-            point = 0;
-        }
-        
-        /*PASSO LA STRINGA AL CLIENT*/
-        //printf("parola:%s, punti:%d\n",input,point);
-        //SYSC(retvalue,write(client_fd,score,strlen(score)),"nella comunicazione del punteggio");
-        Send_Message(client_fd,score,MSG_PUNTI_PAROLA);
-        /*AGGIUNGO IL PUNTEGGIO ATTUALE AL TOTALE*/
-        highscore+=point;
-        free(score);
-    }
-    char score[buff_size];
-    sprintf(score,"hai totalizzato %d punti in tutta la partita\n",highscore);
-    /*sincronizzo client e server*/
-    usleep(2);
-    Send_Message(client_fd,score,MSG_PUNTI_FINALI);
-    //SYSC(retvalue,write(client_fd,score,buff_size),"nella comunicazione del punteggio totale");
-    return;
-}
-
-/*THREAD CHE SCORRE IL DIZIONARIO PER CONTROLLARE LA VALIDITà DELLA PAROLA*/
-void* Pignoler(void* args){
-    int retvalue;
-    /*recupero gli argomenti passati al server*/
-    char* input = (char*) args;
-    //print_hash_table(Dictionary,TABLE_SIZE);
-    
-    /*LEGGO DALLA CODA CONTENENTE IL DIZIONARIO*//*COSTA TROPPO FARE UNA READ VOLTA PER VOLTA*/
-    if(search_string(Dictionary,input,TABLE_SIZE)!=-1){
-        writef(retvalue,"esco con 0");
-        pthread_exit((void*)0);
-        return NULL;
-    }
-    /*SE LA RICERCA NON HA SUCCESSO TERMINO IL THREAD E OTTENGO IL VALORE DI RITORNO*/
-    writef(retvalue,"esco con -1");
-    pthread_exit((void*)-1);
-    return NULL;
-}
-
-void Build_Dictionary(Hash_Entry* table,char* path_to_dictionary){
-    ssize_t n_read;
-    int dizionario_fd;
-    
-    char* token;
-    int retvalue;
-    struct stat st;
-    SYSC(retvalue,stat(path_to_dictionary,&st),"nella stat");
-    /*APRO IL DIZIONARIO IN SOLA LETTURA*/
-    SYSC(dizionario_fd,open(path_to_dictionary,O_RDONLY),"nell'apertura del dizionario");
-    /*LEGGO TUTTO IL CONTENUTO DEL DIZIONARIO*/
-    char buffer[st.st_size];
-    SYSC(n_read,read(dizionario_fd,buffer,st.st_size),"nella lettura dal dizionario");
-    while(n_read>0){
-        // Null-termina il buffer per garantire che sia una stringa valida
-        token = strtok(buffer,"\n");
-        while(token != NULL){
-            /*INSERISCO LA PRIMA PAROLA NELLA LISTA*/
-            Caps_Lock(token);
-            insert_string(table,token,TABLE_SIZE);
-            /*TOKENIZZO PER CERCARE LA PROSSIMA*/
-            token = strtok(NULL,"\n");
-        }
-        
-        SYSC(n_read,read(dizionario_fd,buffer,buff_size),"nella lettura dal dizionario");
-
-    }
-    SYSC(retvalue,close(dizionario_fd),"nella chiusura del file descriptor");
-    return;
 }
