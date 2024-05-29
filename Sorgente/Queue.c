b4d5e648e468b000aa24be17b1f650e20568e873
TaleWind <matteoicy2@gmail.com>
2024-05-29 15:38:57 +0200

    Aggiustata Struttura dati

--- Sorgente/Queue.c
+++ Sorgente/Queue.c
@@ -15,9 +15,6 @@ void WL_Push(Word_List* wl,char* word){
     //alloco spazio per la parola
     el->word = (char*)malloc(strlen(word)+1);
     //registro il thread che gestisce l'utente
-    el->handler = pthread_self();
-    //imposto a 0 il punteggio dell'utente
-    el->points = 0;
     //printf("%s",word);
     strcpy(el->word,word);
     /*faccio puntare l'elemento alla testa della lista*/
@@ -27,24 +24,7 @@ void WL_Push(Word_List* wl,char* word){
     return;
 }
 
-void WL_Push_Thread(Word_List* wl,char* word, int socket_fd){
-    Word_Node* el = (Word_Node*)malloc(sizeof(Word_Node));
-    //alloco spazio per la parola
-    el->word = (char*)malloc(strlen(word)+1);
-    //registro il thread che gestisce l'utente
-    el->handler = pthread_self();
-    //imposto a 0 il punteggio dell'utente
-    el->points = 0;
-    //imposto il scoket di comunicazione
-    el->client_fd = socket_fd;
-    //printf("%s",word);
-    strcpy(el->word,word);
-    /*faccio puntare l'elemento alla testa della lista*/
-    el->next = *wl;
-    /*faccio puntare la testa della lista all'elemento*/
-    *wl= el;
-    return;
-}
+
 
 /*ESTRAGGO L'ELEMENTO IN TESTA ALLA LISTA*/
 char* WL_Pop(Word_List* wl){
@@ -76,7 +56,7 @@ char* WL_Peek(Word_List wl){
 }
 
 /*COMUNICO IL GESTORE DELLA TESTA DELLA LISTA*/
-pthread_t WL_Peek_Hanlder(Word_List wl){
+pthread_t Player_Peek_Hanlder(Player_List wl){
     if (wl == NULL)return -1;
     return wl->handler;
 }
@@ -95,44 +75,11 @@ int WL_Find_Word(Word_List wl,char* word){
     return WL_Find_Word(wl->next,word);
 }
 
-int WL_Update_Score(Word_List wl,pthread_t gestore,int new_score){
-    //caso base
-    if (wl == NULL) return 0;
-    //caso base
-    if(wl->handler == gestore){
-        wl->points+= new_score;
-        return 0;
-    }
-    //caso ricorsivo
-    return WL_Update_Score(wl->next,gestore,new_score);
-}
-//ripensare
-int WL_Retrieve_Score(Word_List wl,pthread_t gestore){
-    //caso base
-    if (wl == NULL)return -1;
-    //caso base
-    if (wl->handler == gestore){
-        return wl->points;
-    }
-    //caso ricorsivo
-    return WL_Retrieve_Score(wl->next,gestore);
-}
 
-char* WL_Retrieve_User(Word_List wl,pthread_t gestore){
-    //caso base
-    if (wl == NULL)return "";
-    //caso base
-    if (wl->handler == gestore){
-        return wl->word;
-    }
-    //caso ricorsivo
-    return WL_Retrieve_User(wl->next,gestore);
-}
-
-int WL_Splice(Word_List* wl,char* word){
+int Player_Splice(Player_List* wl,char* word){
     if (wl == NULL )return -1;
-    Word_Node* prev = NULL;
-    Word_Node* current = *(wl);
+    Player_Node* prev = NULL;
+    Player_Node* current = *(wl);
     //controllo se il prossimo nodo è quello che cerco
     while(current !=NULL && strcmp(current->word,word)!=0){
         prev = current;
@@ -165,8 +112,98 @@ int Print_WList(Word_List wl){
 }
 
 //prendi il client_fd
-int WL_Retrieve_Socket(Word_List wl,pthread_t gestore){
+int Player_Retrieve_Socket(Player_List wl,pthread_t gestore){
     if (wl == NULL)return -1;
     if(wl->handler == gestore)return wl->client_fd;
-    return WL_Retrieve_Socket(wl->next,gestore);
+    return Player_Retrieve_Socket(wl->next,gestore);
+}
+
+void Player_Push_Thread(Player_List* wl,char* word, int socket_fd){
+    Player_Node* el = (Player_Node*)malloc(sizeof(Player_Node));
+    //alloco spazio per la parola
+    el->word = (char*)malloc(strlen(word)+1);
+    //registro il thread che gestisce l'utente
+    el->handler = pthread_self();
+    //imposto a 0 il punteggio dell'utente
+    el->points = 0;
+    //imposto il scoket di comunicazione
+    el->client_fd = socket_fd;
+    //printf("%s",word);
+    strcpy(el->word,word);
+    /*faccio puntare l'elemento alla testa della lista*/
+    el->next = *wl;
+    /*faccio puntare la testa della lista all'elemento*/
+    *wl= el;
+    return;
+}
+
+/*CONTO GLI ELEMENTI DELLA LISTA*/
+int Player_Size(Player_List wl){
+    /*caso base*/
+    if(wl == NULL )return 0;
+    /*chiamata ricorsiva*/
+    return 1+Player_Size(wl->next);
+}
+
+int Player_Update_Score(Player_List wl,pthread_t gestore,int new_score){
+    //caso base
+    if (wl == NULL) return 0;
+    //caso base
+    if(wl->handler == gestore){
+        wl->points+= new_score;
+        return 0;
+    }
+    //caso ricorsivo
+    return Player_Update_Score(wl->next,gestore,new_score);
+}
+//ripensare
+int Player_Retrieve_Score(Player_List wl,pthread_t gestore){
+    //caso base
+    if (wl == NULL)return -1;
+    //caso base
+    if (wl->handler == gestore){
+        return wl->points;
+    }
+    //caso ricorsivo
+    return Player_Retrieve_Score(wl->next,gestore);
+}
+
+char* Player_Retrieve_User(Player_List wl,pthread_t gestore){
+    //caso base
+    if (wl == NULL)return "";
+    //caso base
+    if (wl->handler == gestore){
+        return wl->word;
+    }
+    //caso ricorsivo
+    return Player_Retrieve_User(wl->next,gestore);
+}
+
+/*ESTRAGGO L'ELEMENTO IN TESTA ALLA LISTA*/
+char* Player_Pop(Player_List* wl){
+    if (wl == NULL) return "lista vuota";
+    /*copio la stringa del nodo in testa per restituirla*/ 
+    char* tword = (*wl)->word;
+    /*creo un nodo temporaneo*/
+    Player_Node* temp = *wl;
+    /*faccio puntare la testa della lista al prossimo elemento*/
+    *wl = (*wl)->next;
+    /*dealloco il nodo temporaneo*/
+    free(temp);
+
+    return tword;
+}
+
+int Player_Find_Word(Player_List wl,char* word){
+    //int retvalue;
+    /*caso base*/
+    if(wl == NULL)return -1;
+    /*CONTROLLO DI AVER TROVATO LA PAROLA CHE CERCAVO*/
+    if (strcmp(wl->word,word)==0) return 0;
+
+    //if (wl->next == NULL)return -1;
+    
+    
+    /*chiamata ricorsiva*/
+    return Player_Find_Word(wl->next,word);
 }
